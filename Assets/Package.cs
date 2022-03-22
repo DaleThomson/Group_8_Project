ff6db80d9b1bb601d6718b2d2d15ac1c2964972f
dale2707 <dalethomson2707@gmail.com>
2022-03-22 15:27:53 +0000

    Conveyor Belt System

--- Assets/Package.cs
+++ Assets/Package.cs
@@ -6,7 +6,10 @@ public class Package : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
     float m_thrust = 20f;
-    bool conveyor = false;
+    bool conveyorN = false;
+    bool conveyorE = false;
+    bool conveyorS = false;
+    bool conveyorW = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,19 +18,58 @@ public class Package : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Conveyor")
+        if (other.tag == "ConveyorN")
         {
-            conveyor = true;
+            conveyorN = true;
         }
+        if (other.tag == "ConveyorE")
+        {
+            conveyorE = true;
+        }
+        if (other.tag == "ConveyorS")
+        {
+            conveyorS = true;
+        }
+        if (other.tag == "ConveyorW")
+        {
+            conveyorW = true;
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        conveyorN = false;
+        conveyorE = false;
+        conveyorS = false;
+        conveyorW = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(conveyor)
+        if (conveyorN)
         {
+            m_Rigidbody.rotation = Quaternion.identity;
             m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
             m_Rigidbody.AddForce(transform.forward * m_thrust);
+        }
+        if (conveyorE)
+        {
+            m_Rigidbody.rotation = Quaternion.identity;
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            m_Rigidbody.AddForce(transform.right * m_thrust);
+        }
+        if (conveyorS)
+        {
+            m_Rigidbody.rotation = Quaternion.identity;
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            m_Rigidbody.AddForce(transform.forward * -m_thrust);
+        }
+        if (conveyorW)
+        {
+            m_Rigidbody.rotation = Quaternion.identity;
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            m_Rigidbody.AddForce(transform.right * -m_thrust);
         }
     }
 }

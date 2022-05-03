80e5d30a925ea769009d74a1c45c8bb10f4e5ac3
Dale Thomson <Dalethomson2707@gmail.com>
2022-05-03 17:44:39 +0100

    All Workers Desaturate, Conveyor Belts Reimplemented & Table Jump Resets

--- Assets/Point.cs
+++ Assets/Point.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Point : MonoBehaviour
 {
     public Collider m_collider;
-    private float checkTimer = 5;
+    public float checkTimer;
+    public float resetTimer;
+    bool on = true;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        checkTimer -= Time.deltaTime;
-        if (checkTimer <= 5)
+        if (!on)
         {
+            checkTimer -= Time.deltaTime;
+        }
+        if (checkTimer <= 0)
+        {
+            on = true;
             m_collider.enabled = true;
+            checkTimer = resetTimer;
         }
     }
 
@@ -26,6 +32,7 @@ public class Point : MonoBehaviour
     {
         if (other.tag == "Finished")
         {
+            on = false;
             m_collider.enabled = false;
         }
     }
